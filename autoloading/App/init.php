aa1b4992a33cc468e92bb2987412234877fe482b
Yandiio <yandifenanda95@gmail.com>
2020-07-17 04:49:07 +0700

    alternative method for autoload

--- autoloading/App/init.php
+++ autoloading/App/init.php
@@ -1,7 +1,7 @@
 <?php 
 
 /* using system php library
-// spl_autoload_register(function($class) {
+// spl_autoload_register(function($class) { 
 //     require_once __DIR__ . '/Produk/' . $class . '.php'; 
 // });
 
